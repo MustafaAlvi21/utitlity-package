2df1d6b6e8b2dd4a1e11626b0607eb1213cb923d
MustafaAlvi21 <mustafaalvi21@gmail.com>
2023-05-11 05:00:48 +0500

    'update'

--- index.js
+++ index.js
@@ -186,18 +186,18 @@ class Utility {
 
 
 // const myUtility = new Utility("f253d57f-1740-4603-9697-2dc1399eef73");
-// const myUtility = new Utility("588defcd-92ad-4891-93c8-ddd5bf41ea9d");
-// myUtility.init();
+const myUtility = new Utility("588defcd-92ad-4891-93c8-ddd5bf41ea9d");
+myUtility.init();
 
-// myUtility.nftForWallet("0xf7f9e7be5971dd17563dcbaa745975c0fb919669", 1);
-// myUtility.sftForWallet("0xf7f9e7be5971dd17563dcbaa745975c0fb919669", 1);
-// myUtility.tokensForWallet("0xf7f9e7be5971dd17563dcbaa745975c0fb919669", 1);
-// myUtility.getWallet("address");
-// myUtility.getWallets(1);
-// myUtility.createWallet(1, "test-1");
-// myUtility.updateWallet("42e9e745-fb71-4515-8f0a-db43ba8ec7fe", "test-420", true);
-// myUtility.deleteWallet("42e9e745-fb71-4515-8f0a-db43ba8ec7fe");
-// myUtility.getSignature("0xda8d71c98b395d6ab86959bd64ece07cd2274411", "hello");
+myUtility.nftForWallet("0xf7f9e7be5971dd17563dcbaa745975c0fb919669", 1);
+myUtility.sftForWallet("0xf7f9e7be5971dd17563dcbaa745975c0fb919669", 1);
+myUtility.tokensForWallet("0xf7f9e7be5971dd17563dcbaa745975c0fb919669", 1);
+myUtility.getWallet("address");
+myUtility.getWallets(1);
+myUtility.createWallet(1, "test-1");
+myUtility.updateWallet("42e9e745-fb71-4515-8f0a-db43ba8ec7fe", "test-420", true);
+myUtility.deleteWallet("42e9e745-fb71-4515-8f0a-db43ba8ec7fe");
+myUtility.getSignature("0xda8d71c98b395d6ab86959bd64ece07cd2274411", "hello");
 
 // myUtility.readTx("0","hello","0xda8d71c98b395d6ab86959bd64ece07cd2274411","[]","hello");
 // myUtility.createTx("0","0xda8d71c98b395d6ab86959bd64ece07cd2274411","0xda8d71c98b395d6ab86959bd64ece07cd2274411","ppp","0xda8d71c98b395d6ab86959bd64ece07cd2274411","[]","ppp","120","678687");
