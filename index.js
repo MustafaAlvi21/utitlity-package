e46efaaff7ca4c1d9387c328794790fddb33ac0b
MustafaAlvi21 <mustafaalvi21@gmail.com>
2023-05-16 16:57:12 +0500

    'update'

--- index.js
+++ index.js
@@ -1,46 +1,11 @@
 const axios = require("axios");
-const {
-    nftforwallet,
-    sftforwallet,
-    tokensforwallet,
-    getwallet,
-    getwallets,
-    createwallet,
-    updatewallet,
-    deletewallet,
-    signature
-} = require("./apis/1_wallets");
-const {
-    readtx,
-    createtx,
-    createmetatx,
-    getsingletx,
-    gettxofwallet,
-    gettxstatus
-} = require("./apis/2_transactions");
-const {
-    resynctokens
-} = require("./apis/3_tokens");
-const {
-    resynccontractmetadata,
-    getcontractnfts
-} = require("./apis/4_contracts");
-const {
-    getnamespaces,
-    createnamespaces,
-    getmetadataitems,
-    updatemetadataitem,
-    createmetadataitem,
-    getsinglemetadataitem,
-    deletemetadataitem
-} = require("./apis/5_metadata");
-const {
-    getnfts,
-    resyncnfts
-} = require("./apis/6_nfts");
-const {
-    updateresyncnfts
-} = require("./apis/7_sfts");
+const { resynctokens } = require("./apis/3_tokens");
+const { updateresyncnfts } = require("./apis/7_sfts");
+const { getnfts, resyncnfts } = require("./apis/6_nfts");
+const { resynccontractmetadata, getcontractnfts } = require("./apis/4_contracts");
+const { readtx, createtx, createmetatx, getsingletx, gettxofwallet, gettxstatus } = require("./apis/2_transactions");
+const { nftforwallet, sftforwallet, tokensforwallet, getwallet, getwallets, createwallet, updatewallet, deletewallet, signature } = require("./apis/1_wallets");
+const { getnamespaces, createnamespaces, getmetadataitems, updatemetadataitem, createmetadataitem, getsinglemetadataitem, deletemetadataitem } = require("./apis/5_metadata");
 
 
 
@@ -52,180 +17,137 @@ class Utility {
     }
 
     init() {
-       axios.defaults.headers.common['accept'] = `'application/json'`;
+        axios.defaults.headers.common['accept'] = `'application/json'`;
         axios.defaults.headers.common['X-API-Key'] = `${this.apikey}`;
     }
 
     // Wallet APIs
     nftForWallet(...params) {
-        nftforwallet(...params)
+        return nftforwallet(...params)
     }
 
     sftForWallet(...params) {
-        sftforwallet(...params)
+        return sftforwallet(...params)
     }
 
     tokensForWallet(...params) {
-        tokensforwallet(...params)
+        return tokensforwallet(...params)
     }
 
     getWallet(...params) {
-        getwallet(...params)
+        return getwallet(...params)
     }
 
     getWallets(...params) {
-        getwallets(...params)
+        return getwallets(...params)
     }
 
     createWallet(...params) {
-        createwallet(...params)
+        return createwallet(...params)
     }
 
     updateWallet(...params) {
-        updatewallet(...params)
+        return updatewallet(...params)
     }
 
     deleteWallet(...params) {
-        deletewallet(...params)
+        return deletewallet(...params)
     }
 
     getSignature(...params) {
-        signature(...params)
+        return signature(...params)
     }
-    
-    
+
+
     // Transaction APIs
     readTx(...params) {
-        readtx(...params)
+        return readtx(...params)
     }
-    
+
     createTx(...params) {
-        createtx(...params)
+        return createtx(...params)
     }
-    
+
     createMetaTx(...params) {
-        createmetatx(...params)
+        return createmetatx(...params)
     }
-    
+
     getSingleTx(...params) {
-        getsingletx(...params)
+        return getsingletx(...params)
     }
-    
+
     getTxOfWallet(...params) {
-        gettxofwallet(...params)
+        return gettxofwallet(...params)
     }
-    
+
     getTxStatus(...params) {
-        gettxstatus(...params)
+        return gettxstatus(...params)
     }
 
 
     // Tokens APIs
     resyncTokens(...params) {
-        resynctokens(...params)
+        return resynctokens(...params)
     }
-    
+
 
     // Contracts APIs
     resyncContractMetadata(...params) {
-        resynccontractmetadata(...params)
+        return resynccontractmetadata(...params)
     }
 
     getContractNfts(...params) {
-        getcontractnfts(...params)
+        return getcontractnfts(...params)
     }
 
 
     // Metadata APIs
     getNamespaces(...params) {
-        getnamespaces(...params)
+        return getnamespaces(...params)
     }
 
     createNamespaces(...params) {
-        createnamespaces(...params)
+        return createnamespaces(...params)
     }
 
     getMetadataItems(...params) {
-        getmetadataitems(...params)
+        return getmetadataitems(...params)
     }
 
     updateMetadataItem(...params) {
-        updatemetadataitem(...params)
+        return updatemetadataitem(...params)
     }
 
     createMetadataItem(...params) {
-        createmetadataitem(...params)
+        return createmetadataitem(...params)
     }
 
     getSingleMetadataItem(...params) {
-        getsinglemetadataitem(...params)
+        return getsinglemetadataitem(...params)
     }
 
     deleteMetadataItem(...params) {
-        deletemetadataitem(...params)
+        return deletemetadataitem(...params)
     }
 
 
     // Nfts APIs
     getNfts(...params) {
-        getnfts(...params)
+        return getnfts(...params)
     }
 
     resyncNfts(...params) {
-        resyncnfts(...params)
+        return resyncnfts(...params)
     }
-    
-    
+
+
     // Sfts APIs
     updateResyncnfts(...params) {
-        updateresyncnfts(...params)
+        return updateresyncnfts(...params)
     }
 
 
 }
-
-
-// const myUtility = new Utility("f253d57f-1740-4603-9697-2dc1399eef73");
-const myUtility = new Utility("588defcd-92ad-4891-93c8-ddd5bf41ea9d");
-myUtility.init();
-
-myUtility.nftForWallet("0xf7f9e7be5971dd17563dcbaa745975c0fb919669", 1);
-myUtility.sftForWallet("0xf7f9e7be5971dd17563dcbaa745975c0fb919669", 1);
-myUtility.tokensForWallet("0xf7f9e7be5971dd17563dcbaa745975c0fb919669", 1);
-myUtility.getWallet("address");
-myUtility.getWallets(1);
-myUtility.createWallet(1, "test-1");
-myUtility.updateWallet("42e9e745-fb71-4515-8f0a-db43ba8ec7fe", "test-420", true);
-myUtility.deleteWallet("42e9e745-fb71-4515-8f0a-db43ba8ec7fe");
-myUtility.getSignature("0xda8d71c98b395d6ab86959bd64ece07cd2274411", "hello");
-
-// myUtility.readTx("0","hello","0xda8d71c98b395d6ab86959bd64ece07cd2274411","[]","hello");
-// myUtility.createTx("0","0xda8d71c98b395d6ab86959bd64ece07cd2274411","0xda8d71c98b395d6ab86959bd64ece07cd2274411","ppp","0xda8d71c98b395d6ab86959bd64ece07cd2274411","[]","ppp","120","678687");
-// myUtility.createMetaTx("0","0xda8d71c98b395d6ab86959bd64ece07cd2274411","0xda8d71c98b395d6ab86959bd64ece07cd2274411","0xda8d71c98b395d6ab86959bd64ece07cd2274411","ppp","[]","iii","1231","9798");
-// myUtility.getSingleTx("transaction_id");
-// myUtility.getTxOfWallet("42e9e745-fb71-4515-8f0a-db43ba8ec7fe","1","8");
-// myUtility.getTxStatus("transaction_id");
-
-// myUtility.resyncTokens("0xda8d71c98b395d6ab86959bd64ece07cd2274411", "1", "0xda8d71c98b395d6ab86959bd64ece07cd2274411");
-
-// myUtility.resyncContractMetadata("0xda8d71c98b395d6ab86959bd64ece07cd2274411", "1");
-// myUtility.getContractNfts("0xda8d71c98b395d6ab86959bd64ece07cd2274411", "1", "1", "1");
-
-// myUtility.getNamespaces("1", "2");
-// myUtility.createNamespaces("namespace = null", "description");
-// myUtility.getMetadataItems("namespace = null", "page = null", "limit");
-// myUtility.updateMetadataItem("namespace = null", "id = null", "name = null", "image = null", "description = null", "attributes = null");
-// myUtility.createMetadataItem("namespace = null", "id = null", "name = null", "image = null", "description = null", "attributes = null");
-// myUtility.getSingleMetadataItem("namespace = null", "id = null");
-// myUtility.deleteMetadataItem("namespace = null", "id = null");
-
-// myUtility.getNfts("token_id = null", "contract_address = null", "network_id = null");
-// myUtility.resyncNfts("address = null", "network = null", "token_ids = null");
-
-// myUtility.updateResyncnfts("address = null", "network = null", "owner = null", "token_ids = null");
-
-
-
 
 
 
